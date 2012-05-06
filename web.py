1e353b12d1d526fa8aa63341af7888379f8d3ced
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-05-06 22:50:58 +0200

    Starting point for an python web app using: Flask, SQLAlchemy

---
+++ web.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template, redirect, url_for, request, make_response
+from flaskext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+if not os.environ.get('PROD'):
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.debug = True
+
+db = SQLAlchemy(app)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
