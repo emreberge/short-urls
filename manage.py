1dce9737a9aa498681d4464fae703db124aaa160
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-05-07 21:07:31 +0200

    Renames the db creation method and adds a db drop_all method

--- manage.py
+++ manage.py
@@ -4,8 +4,12 @@ from web import db, app
 manager = Manager(app)
 
 @manager.command
-def createDbSchema():
+def create_all():
     db.create_all()
+    
+@manager.command
+def drop_all():
+    db.drop_all()
 
 if __name__ == "__main__":
     manager.run()
