2126fd566ad06772df7b63df0cca030ab9c4c5cd
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-05-13 15:56:29 +0200

    Refactoring removing unused mthods and changed test names to be more presistant.

--- test.py
+++ test.py
@@ -23,10 +23,10 @@ class Test_Web_App(unittest.TestCase):
 
 # Positive adding/redirecting tests
     
-    def test_craeting_with_valid_url_should_redirect_to_the_same_url(self):
-        self.redirect_works_for('http://emreberge.com', 'http://emreberge.com')
+    def test_valid_url(self):
+        self.url_redirects_to('http://emreberge.com', 'http://emreberge.com')
         
-    def redirect_works_for(self, test_url, redirect_url):
+    def url_redirects_to(self, test_url, redirect_url):
         response_data = self.app.post('/', data=dict(url=test_url)).data
         self.response_data_is_json(response_data, DB_FIRST_INDEX)
         self.response_redirects_to(self.app.get(DB_FIRST_INDEX), redirect_url)
@@ -38,82 +38,78 @@ class Test_Web_App(unittest.TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.headers['Location'], redirect_url)
     
-    def test_should_work_with_https(self):
-        self.redirect_works_for('https://google.com', 'https://google.com')
+    def test_https_url(self):
+        self.url_redirects_to('https://google.com', 'https://google.com')
 
-    def test_should_work_with_url(self):
-        self.redirect_works_for('72.26.203.99/939/', 'http://72.26.203.99/939/')
+    def test_ip_address(self):
+        self.url_redirects_to('72.26.203.99/939/', 'http://72.26.203.99/939/')
 
-    def test_shoudl_work_with_relative_url(self):
-        self.redirect_works_for('emreberge.com', 'http://emreberge.com')
+    def test_relative_url(self):
+        self.url_redirects_to('emreberge.com', 'http://emreberge.com')
         
-    def test_should_work_for_urls_with_parameters(self):
-        self.redirect_works_for('http://www.youtube.com/watch?v=oHg5SJYRHA0', 'http://www.youtube.com/watch?v=oHg5SJYRHA0');
+    def test_urls_with_parameters(self):
+        self.url_redirects_to('http://www.youtube.com/watch?v=oHg5SJYRHA0', 'http://www.youtube.com/watch?v=oHg5SJYRHA0');
         
 
 # Negative adding tests
         
-    def test_mail_links_should_return_400(self):
-        self.adding_should_fail_with_error_for('mailto:spam@spamer.com', 400)
+    def test_mailto_should_fail_with_400(self):
+        self.adding_should_fail_with_error('mailto:spam@spamer.com', 400)
         
-    def adding_should_fail_with_error_for(self, url_to_add, error_code): 
+    def adding_should_fail_with_error(self, url_to_add, error_code): 
         response = self.app.post('/', data=dict(url=url_to_add))
         self.assertEqual(response.status_code, error_code)
 
-    def test_javascript_should_return_400(self):
-        self.adding_should_fail_with_error_for('javascript:alert(\'BAM!\')', 400)
+    def test_javascript_should_fail_with_400(self):
+        self.adding_should_fail_with_error('javascript:alert(\'BAM!\')', 400)
         
-    def test_non_valid_url_should_return_400(self):
-        self.adding_should_fail_with_error_for('this/url/is/not/valid', 400)
+    def test_non_valid_url_should_fail_with_400(self):
+        self.adding_should_fail_with_error('this/url/is/not/valid', 400)
 
 
 # Negative redirect tests
 
-    def test_retrieving_non_existing_short_url_should_result_404(self):        
+    def test_non_existing_short_url_should_fail_with_404(self):        
         self.redirect_fails_with_error('/xDseF', 404)
         
     def redirect_fails_with_error(self, test_url, error_code):
         response = self.app.get('test_url')
         self.assertEqual(response.status_code, error_code)
             
-    def test_retrieving_malformated_short_url_char_should_result_404(self):
+    def test_malformated_short_url_char_should_faile_with_404(self):
         self.redirect_fails_with_error('/*', 404)
             
-    def test_retrieving_malformated_short_url_string_should_result_404(self):
+    def test_malformated_short_url_string_should_fail_with_404(self):
         self.redirect_fails_with_error('/*#%', 404)
 
         
 class Test_Url(unittest.TestCase):
-    
-    def check_redirect_response(self, response, expected_url):
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.headers['Location'], expected_url)
-            
-    def test_creating_with_id_1_returns_base64_encoded_short_url(self):
+                
+    def test_url_with_id_1(self):
         url = Url('http://emreberge.com');
         url.id = 1;
         self.assertEqual(url.short_url(), 'B')
         
-    def test_creating_with_id_23_returns_base64_encoded_short_url(self):
+    def test_url_with_id_23(self):
         url = Url('http://emreberge.com');
         url.id = 23;
         self.assertEqual(url.short_url(), 'X')
         
-    def test_getting_id_for_short_url_B(self):
+    def test_short_url_B(self):
         self.assertEqual(Url.id_for_short_url('B'), 1)
     
-    def test_getting_id_for_short_url_X(self):
+    def test_short_url_X(self):
         self.assertEqual(Url.id_for_short_url('X'), 23)
                 
 class Test_b64(unittest.TestCase):
     
-    def test_1(self):
+    def test_encode_decode_1(self):
         self.assertEqual(num_decode(num_encode(1)),1)
         
-    def test_encode_1234123(self):
+    def test_encode_decode_1234123(self):
         self.assertEqual(num_decode(num_encode(1234123)),1234123)
         
-    def test_decode_ilegal_char_should_reise_illegal_argument_exeption(self):
+    def test_decode_ilegal_char_should_raise_value_error(self):
         with self.assertRaises(ValueError):
             num_decode('*')
         
