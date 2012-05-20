85d5bdde339ab0b0ac640ba27a0f2782f5f8be4c
Emre Berge Ergenekon <eergenekon@splunk.com>
2012-05-20 09:58:24 +0200

    SHort urls are now added to a table on the main page. Refactoring: Extracting methods to make code more readable.

--- static/short_urls.js
+++ static/short_urls.js
@@ -1,21 +1,39 @@
+// Executed when the document has loaded.
 $(document).ready(function() {
-    $("#shortenUrlForm").submit(function(event) {
-        event.preventDefault();
-
-        var $form = $(this),
-        term = $form.find('input[name="url_address"]').val(),
-        url = $form.attr('action');
-
-        $.post(url, {
-            url_address: term
-        },
-        function(data) {
-            var content = data.short_url;
-            $("#result").empty().append(content);
-        }
-        ).error(
-        function() {
-            alert("".concat('The url: ', term, ' is not valid or an allowed address.'));
-        });
-    });
+    initilizeShortenUrlForm();
 });
+
+function initilizeShortenUrlForm() {
+    $("#shortenUrlForm").submit(submitSortenUrlForm);
+}
+
+function submitSortenUrlForm(event) {
+    event.preventDefault();
+    var form = retrieveAddressAndActionFromForm($(this));
+
+    $.post(form.action, form.params)
+    .success(function(data) {
+        addShortUrlToTable(data.short_url, form.params.url_address);
+    })
+    .error(function() {
+       displayErrorForUrl(form.params.url_address);
+    });
+
+}
+
+function retrieveAddressAndActionFromForm($form) {
+    return {
+        action: $form.attr('action'),
+        params: {
+            url_address: $form.find('input[name="url_address"]').val()
+        }
+    }
+}
+
+function addShortUrlToTable(shortUrl, longUrl) {
+    $("#result").prepend("<tr><td>" + longUrl + "</td><td>" + shortUrl + "</td><tr>");
+}
+
+function displayErrorForUrl(url) {
+    alert("".concat('The url: "', url, '" is not valid or an allowed address.'));
+}
